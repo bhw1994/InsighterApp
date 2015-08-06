27f260045cb336752c6e0ae0eed9e743d76f63f9
bhw1994 <bhw1994@gmail.com>
2015-08-06 09:40:11 +0900

    commit test

--- www/js/DbAccessor/DbAccessor.js
+++ www/js/DbAccessor/DbAccessor.js
@@ -3,5 +3,18 @@
  */
 function DbAccessor()
 {
-    
+    this.db;
+    this.init();
+}
+DbAccessor.prototype.init=function(){
+
+    switch (arguments.length)
+    {
+        case 3:
+            this.db = window.openDatabase(arguments[0], arguments[1], arguments[2], arguments[3]);
+            break;
+        default :
+            this.db = window.openDatabase("test", "1.0", "Test DB", 1000000);
+            break;
+    }
 }
