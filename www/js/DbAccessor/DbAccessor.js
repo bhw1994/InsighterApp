12fa0af31910b3a7d6bc606e49192e75e3b06cab
bhw1994 <bhw1994@gmail.com>
2015-08-11 13:23:46 +0900

    commit test

--- www/js/DbAccessor/DbAccessor.js
+++ www/js/DbAccessor/DbAccessor.js
@@ -27,133 +27,13 @@ DbAccessor.prototype.init=function(){
     switch (arguments.length)
     {
         case 4:
-            this.db = window.openDatabase(arguments[0], arguments[1], arguments[2], arguments[3]);
+            self.db = window.openDatabase(arguments[0], arguments[1], arguments[2], arguments[3]);
             break;
+
         default :
-            this.db = window.openDatabase(self.dbName, "1.0", "Test DB", 1000000);
+            self.db = window.openDatabase(self.dbName, "1.0", "Test DB", 1000000);
             break;
     }
-}
-
-DbAccessor.prototype.changeQuery=function(){
-    switch (arguments.length)
-    {
-        case 1:
-
-            this.db.transaction(arguments[0], errorCB, successCB);
-            break;
-        case 2:
-            this.db.transaction(arguments[0], errorCB, arguments[1]);
-            break;
-        case 3:
-            this.db.transaction(arguments[0], arguments[1], arguments[2]);
-            break;
-        default :
-            alert("argument number error");
-            break;
-
-    }
-    //this.db.transaction(populateDB, errorCB, successCB);
-};
-DbAccessor.prototype.resultQuery=function(){
-    switch (arguments.length)
-    {
-        case 1:
-            this.db.transaction(arguments[0], errorCB);
-            break;
-        case 2:
-            this.db.transaction(arguments[0], arguments[1]);
-            break;
-        default :
-            alert("argument number error");
-            break;
-
-    }
-    //db.transaction(queryDB, errorCB);
-};
-
-DbAccessor.prototype.createTable=function(tableName,valueArr)
-{
-
-    var self=this;
-
-    var str="CREATE TABLE IF NOT EXISTS "+tableName+" ("+valueArr.join(",")+")";
-    var queryFunc=self.makeQueryFunc(str);
-    self.changeQuery(queryFunc);
-
-}
-DbAccessor.prototype.dropTable=function(tableName)
-{
-    var self=this;
-    var queryFunc=self.makeQueryFunc("DROP TABLE IF EXISTS "+tableName);
-    self.changeQuery(queryFunc);
-}
-DbAccessor.prototype.insert=function(tableName,valueArr){
-    var self=this;
-
-    var str="insert into "+tableName+" values(\""+valueArr.join("\",\"")+"\")";
-
-    var queryFunc=self.makeQueryFunc(str);
-    self.changeQuery(queryFunc);
-};
-DbAccessor.prototype.select=function(){
-    var self=this;
-
-    var queryFunc;
-    switch(arguments.length)
-    {
-        case 1:
-            queryFunc=self.makeQueryFunc(arguments[0],querySuccess);
-            self.resultQuery(queryFunc);
-            break;
-
-        case 2:
-            var str="select "+arguments[1]+" from "+arguments[0];
-            var queryFunc=self.makeQueryFunc(str,querySuccess);
-            self.resultQuery(queryFunc);
-            break;
-        case 3:
-            var querySuccessFunc=self.makeQuerySuccessFunc(querySuccess,arguments[2]);
-            var str="select "+arguments[1]+" from "+arguments[0];
-            var queryFunc=self.makeQueryFunc(str,querySuccessFunc);
-            self.resultQuery(queryFunc);
-            break;
-
-        default:
-            break;
-
-    }
-}
-DbAccessor.prototype.makeQueryFunc=function()
-{
-
-    var arg=arguments;
-    switch (arguments.length)
-    {
-        case 1:
-
-            return function (tx){
-                tx.executeSql(arg[0]);
-            };
-        case 2:
-            return function(tx){
-                tx.executeSql(arg[0],[],arg[1],errorCB);
-            };
-
-    }
-
-}
-DbAccessor.prototype.makeQuerySuccessFunc=function(arg1,arg2){
-    return function(tx,results){
-        arg1(tx, results);
-        arg2();
-    }
-}
-function populateDB(tx) {
-    tx.executeSql('DROP TABLE IF EXISTS DEMO');
-    tx.executeSql('CREATE TABLE IF NOT EXISTS DEMO (id unique, data)');
-    tx.executeSql('INSERT INTO DEMO (id, data) VALUES (1, "First row")');
-    tx.executeSql('INSERT INTO DEMO (id, data) VALUES (2, "Second row")');
 }
 
 function errorCB(err) {
@@ -165,10 +45,207 @@ function successCB() {
 }
 
 
-function queryDB(tx) {
-    tx.executeSql('SELECT * FROM DEMO', [], querySuccess, errorCB);
+DbAccessor.prototype.transaction=function()
+{
+    var arg=arguments;
+    var self=this;
+
+    switch (arg.length)
+    {
+        case 1:
+            self.db.transaction(arg[0]);
+            break;
+
+        case 2:
+            self.db.transaction(arg[0],arg[1]);
+            break;
+
+        case 3:
+            self.db.transaction(arg[0],arg[1],arg[2]);
+            break;
+    }
+//this.db.transaction(query,error,succeess);
 }
 
+DbAccessor.prototype.makeExecuteSql=function()
+{
+
+    var arg=arguments;
+    switch (arguments.length)
+    {
+        case 1:
+            return function (tx){
+                tx.executeSql(arg[0],successCB,errorCB);
+            };
+        case 2:
+            return function(tx){
+                tx.executeSql(arg[0],[],arg[1],errorCB);
+            };
+
+        case 3:
+            return function(tx){
+                tx.executeSql(arg[0],[],arg[1],arg[2]);
+            };
+
+
+    }
+
+}
+DbAccessor.prototype.makeQuerySuccess=function(logFunc,dfd){
+    return function(tx,results){
+        logFunc(tx, results);
+        dfd.resolve(results);
+
+    }
+}
+DbAccessor.prototype.select=function()
+{
+    var self=this;
+    var arg=arguments;
+
+    var dfd= new Deferred();
+
+
+    switch(arguments.length)
+    {
+        case 1:
+            var success=dbAccessor.makeQuerySuccess(querySuccess,dfd);
+            var sql=dbAccessor.makeExecuteSql(arg[0],success,errorCB);
+            dbAccessor.transaction(sql,errorCB,successCB);
+            break;
+        case 2:
+            var str="select "+arg[1]+" from "+arg[0];
+            var success=dbAccessor.makeQuerySuccess(querySuccess,dfd);
+            var sql=dbAccessor.makeExecuteSql(str,success,errorCB);
+            dbAccessor.transaction(sql,errorCB,successCB);
+            break;
+    }
+
+
+    return dfd.promise;
+}
+
+
+
+
+
+DbAccessor.prototype.createTable=function(tableName,valueArr)
+{
+
+    var self=this;
+
+    var str="CREATE TABLE IF NOT EXISTS "+tableName+" ("+valueArr.join(",")+")";
+    var sql=self.makeExecuteSql(str);
+    self.transaction(sql);
+}
+DbAccessor.prototype.dropTable=function(tableName)
+{
+    var self=this;
+
+    var str="DROP TABLE IF EXISTS "+tableName;
+    var sql=self.makeExecuteSql(str);
+    self.transaction(sql);
+}
+DbAccessor.prototype.insert=function(tableName,valueArr){
+    var self=this;
+
+    var str="insert into "+tableName+" values(\""+valueArr.join("\",\"")+"\")";
+
+    var sql=self.makeExecuteSql(str);
+    self.transaction(sql);
+
+};
+
+
+DbAccessor.prototype.checkTable=function(tableName){
+    var self=this;
+
+    var str="SELECT name FROM sqlite_master WHERE type='table' AND name='"+tableName+"'";
+    var promise=self.select(str);
+
+    var resultPromise=promise.then(function(results){
+        var _promise;
+        if(self.hasRow(results))
+        {
+            _promise=Promise.resolve();
+        }
+        else
+        {
+            _promise=Promise.reject();
+        }
+        return _promise;
+    })
+    return resultPromise;
+}
+
+DbAccessor.prototype.hasRow=function(results)
+{
+    return results.rows.length;
+}
+
+/*
+DbAccessor.prototype.hasTable(tableName)
+{
+    var self=this;
+    var str="SELECT name FROM sqlite_master WHERE type='table' AND name='"+tableName+"'";
+    var dfd=self.select(str);
+
+}
+
+
+DbAccessor.prototype.downloadTable=function(results)
+{
+    if(!results.rows.length)
+    {
+        var data=downloadWordTable();
+        insertResources(window.sessionStorage.getItem("selectedTable"),data);
+    }
+
+}
+DbAccessor.prototype.showWordTable=function(tableName)
+{
+
+}
+
+
+function addListView(results){
+    for(var index=0;index<results.rows.length;index++) {
+        $("#wordLists").append("<li>" + results.rows[index].word +  "</li>");
+    }
+}
+
+
+
+
+function downloadWordTable() {
+
+    var getData;
+    $.ajax({
+        url: "http://192.168.0.7:9611",
+        async: true,
+        success: function (data) {
+            getData = data;
+        },
+        error: function (e) {
+            getData = null;
+            alert("download error");
+            // $("#test").find("h1").text("error");
+        }
+    });
+    return getData;
+}
+
+*/
+function insertResources(tableName, wordTableResource){
+    var id=0;
+    dbAccessor.createTable(tableName);
+    for(var index in wordTableResource.wordMap){
+        var wordSet = wordTableResource.wordMap[index];
+        id++;
+        dbAccessor.insert(tableName, [id,wordSet.word,wordSet.mean]);
+    }
+
+}
 
 
 
@@ -183,16 +260,60 @@ function querySuccess(tx, results) {
         resultString+="Row = " + i + " ID = " + results.rows.item(i).id + " word =  " + results.rows.item(i).word+" mean =  " + results.rows.item(i).mean+"\n";
     }
     alert(resultString);
-    queryResult=results;
-
 }
 
-function resourceinsert(resource)
-{
 
-
-    dbAccessor.insert();
-
-
-}var queryResult;
 var dbAccessor=new DbAccessor();
+
+
+
+/*
+ DbAccessor.prototype.select=function(){
+ var arg=arguments;
+ var self=this;
+
+ var promise= $.deferred();
+ var queryFunc;
+ switch(arguments.length)
+ {
+ case 1:
+ var querySuccessFunc=self.makeQuerySuccessFunc(querySuccess,null);
+ queryFunc=self.makeQueryFunc(arg[0],querySuccessFunc);
+ self.transaction(queryFunc);
+ break;
+
+ case 2:
+ if(typeof(arg[1])=="function")
+ {
+ var querySuccessFunc=self.makeQuerySuccessFunc(querySuccess,arg[1]);
+ queryFunc=self.makeQueryFunc(arg[0],querySuccessFunc);
+ self.transaction(queryFunc);
+ }
+ else
+ {
+ var str="select "+arg[1]+" from "+arg[0];
+ queryFunc=self.makeQueryFunc(str,querySuccess);
+ self.transaction(queryFunc);
+ }
+
+ break;
+ case 3:
+
+ var querySuccessFunc=self.makeQuerySuccessFunc(querySuccess,arg[2]);
+ var str="select "+arg[1]+" from "+arg[0];
+ var queryFunc=self.makeQueryFunc(str,querySuccessFunc);
+ self.transaction(queryFunc);
+ break;
+
+ default:
+ break;
+
+ }
+
+
+
+
+ return promise.promise();
+
+ }
+ */
